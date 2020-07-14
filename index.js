7a8b28eb4b5376d025ac3c20cb83066dcbe97b86
ryanp <ryanpanimation@gmail.com>
2020-07-13 23:55:33 -0500

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code away!
+const server = require("./server.js");
+
+const PORT = 5000;
+server.listen(5000, () => console.log(`server listening on localhost:`, PORT));
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
+const postRouter = require("./posts/postRouter.js");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+// server.use(logger);
+server.use("/api/posts", postRouter);
+
+server.get("/", (req, res) => {
+  // res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({ message: "it's working", name: "api3-project" });
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request`);
+  next();
+}
 
 module.exports = server;
